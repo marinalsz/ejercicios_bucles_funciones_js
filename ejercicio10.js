bb010cd5bef50ed1e3969b1662e7fe523a6b96c1
Marina <marina.lsz522@gmail.com>
2024-08-19 00:54:47 +0200

    Modificados ejercicios

---
+++ ejercicio10.js
@@ -0,0 +1,28 @@
+//Ejercicio 10
+
+const capitals = {
+  Spain: "Madrid",
+  France: "Paris",
+  Italy: "Rome",
+  Germany: "Berlin",
+  Portugal: "Lisbon",
+  Poland: "Warsaw",
+  Greece: "Athens",
+  Austria: "Vienna",
+  Hungary: "Budapest",
+  Ireland: "Dublin",
+};
+
+function getCapital(country) {
+  if (capitals[country]) {
+    console.log("La capital de " + country + " es " + capitals[country]);
+  } else {
+    console.log(
+      "No tenemos ese país en nuestra base de datos, intentalo con otro."
+    );
+  }
+}
+
+getCapital("Italy");
+getCapital("Poland");
+getCapital("Barbieland");
---
+++ ejercicio7.js
@@ -0,0 +1,26 @@
+// Ejercicio 7
+
+const tracks = [
+  { title: "Enter Sandman", genre: "Metal" },
+  { title: "Back in Black", genre: "Rock" },
+  { title: "Bohemian Rhapsody", genre: "Rock" },
+  { title: "Blinding Lights", genre: "Pop" },
+  { title: "Old Town Road", genre: "Country" },
+  { title: "Smells Like Teen Spirit", genre: "Grunge" },
+  { title: "Bad Guy", genre: "Pop" },
+  { title: "Thunderstruck", genre: "Rock" },
+  { title: "Hotel California", genre: "Rock" },
+  { title: "Stairway to Heaven", genre: "Rock" },
+];
+
+const music = {};
+
+for (const track of tracks) {
+  const genre = track.genre;
+  const title = track.title;
+  if (!music[genre]) {
+    music[genre] = [];
+  }
+  music[genre].push(title);
+}
+console.log(music);
